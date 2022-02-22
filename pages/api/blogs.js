517cb31f15340f9d7d417291465dabf614400f0b
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2022-02-22 18:39:22 +0530

    defined an api path for fetching blogs from firebase

---
+++ pages/api/blogs.js
@@ -0,0 +1,27 @@
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
+
+const db = getFirestore();
+
+export default async function getBlogs(req, res) {
+  const q = query(
+    collection(db, "blogs"),
+    where("userId", "==", "w9XrkpSSyybQB4OdAVkAoRmQg4A2")
+  );
+  const querySnapshot = await getDocs(q);
+  const blogs = [];
+  querySnapshot.forEach((doc) => {
+    blogs.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
+  res.status(200).json(blogs);
+}
