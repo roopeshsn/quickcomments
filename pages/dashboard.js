8a199b375646e7c486ce9159c50e1e4845a420b1
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2022-02-22 18:42:52 +0530

    logic for fetching blogs from api

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,23 @@
 import React from "react";
 import EmptyState from "@/lib/components/EmptyState";
+import { useAuth } from "@/lib/auth";
+import DashboardTemplate from "@/lib/components/DashboardTemplate";
+import useSWR from "swr";
+import fetcher from "@/utils/fetcher";
 
 export default function Dashboard() {
-  return <EmptyState />;
+  const { user } = useAuth();
+  const { data } = useSWR("/api/blogs", fetcher);
+
+  console.log(data);
+
+  if (!user) {
+    return "Loading...";
+  }
+
+  return (
+    <DashboardTemplate>
+      <EmptyState />
+    </DashboardTemplate>
+  );
 }
