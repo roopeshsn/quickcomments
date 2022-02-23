5728103ab4b407e40b1250dc5ce2a220e6bf894d
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2022-02-23 07:23:19 +0530

    wrapped container to center the page content

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -4,6 +4,8 @@ import { useAuth } from "@/lib/auth";
 import DashboardTemplate from "@/lib/components/DashboardTemplate";
 import useSWR from "swr";
 import fetcher from "@/utils/fetcher";
+import SiteTable from "@/lib/components/SiteTable";
+import SiteHeader from "@/lib/components/SiteHeader";
 
 export default function Dashboard() {
   const { user } = useAuth();
@@ -17,7 +19,14 @@ export default function Dashboard() {
 
   return (
     <DashboardTemplate>
-      <EmptyState />
+      {data ? (
+        <>
+          <SiteHeader />
+          <SiteTable blogs={data} />
+        </>
+      ) : (
+        <EmptyState />
+      )}
     </DashboardTemplate>
   );
 }
